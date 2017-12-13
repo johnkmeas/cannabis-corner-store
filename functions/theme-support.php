08233adaf1d6c8e48bcf73acc6bb642b1c0e78cd
johnkmeas <johnkmeas@gmail.com>
2017-12-13 01:46:44 -0800

    basic setup with archive and single product pages, and using foundation grids

--- functions/theme-support.php
+++ functions/theme-support.php
@@ -53,3 +53,53 @@ function joints_theme_support() {
 } /* end theme support */
 
 add_action( 'after_setup_theme', 'joints_theme_support' );
+
+
+// unhook the WooCommerce wrappers
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+// WooCommerce Hook
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<div class="content">';
+}
+
+function my_theme_wrapper_end() {
+  echo '<div>';
+}
+
+// Declare Woocommerce support
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+// Woocommerce short description excerpt
+add_filter('woocommerce_short_description', 'cannabis_woocommerce_short_description', 10, 1);
+function cannabis_woocommerce_short_description($post_excerpt){
+    if (!is_product()) {
+        $post_excerpt = substr($post_excerpt, 0, 50);
+    }
+    return $post_excerpt;
+}
+
+
+// Woocommerce rating stars always
+add_filter('woocommerce_product_get_rating_html', 'your_get_rating_html', 10, 2);
+
+function your_get_rating_html($rating_html, $rating) {
+	if ( $rating > 0 ) {
+	$title = sprintf( __( 'Rated %s out of 5', 'woocommerce' ), $rating );
+	} else {
+	$title = 'Not yet rated';
+	$rating = 0;
+	}
+
+	$rating_html  = '<div class="star-rating" title="' . $title . '">';
+	$rating_html .= '<span style="width:' . ( ( $rating / 5 ) * 100 ) . '%"><strong class="rating">' . $rating . '</strong> ' . __( 'out of 5', 'woocommerce' ) . '</span>';
+	$rating_html .= '</div>';
+	return $rating_html;
+}
